1efe12fee269c2506dd5051f1db69f762852acff
Fabian Grutsch <f.grutsch@gmail.com>
2026-02-23 20:39:31 +0100

    move organization to io.github.fgrutsch

---
+++ core/src/main/scala/io/github/fgrutsch/pekko/persistence/mapdb/journal/JournalRow.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2026 pekko-persistence-mapdb contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.fgrutsch.pekko.persistence.mapdb.journal
+
+import java.util.Comparator
+
+final case class JournalRow(
+    ordering: Long,
+    deleted: Boolean,
+    persistenceId: String,
+    sequenceNr: Long,
+    writer: String,
+    timestamp: Long,
+    manifest: String,
+    eventPayload: Array[Byte],
+    eventSerId: Int,
+    eventSerManifest: String,
+    tags: Set[String]
+)
+
+object JournalRow {
+  val orderingComparator: Comparator[JournalRow] = Comparator.comparingLong(_.ordering)
+  val seqNrComparator: Comparator[JournalRow]    = Comparator.comparingLong(_.sequenceNr)
+}
---
+++ core/src/test/scala/io/github/fgrutsch/pekko/persistence/mapdb/query/TestEventAdapter.scala
@@ -0,0 +1,22 @@
+package io.github.fgrutsch.pekko.persistence.mapdb.query
+
+import org.apache.pekko.persistence.journal.{EventAdapter, EventSeq, Tagged}
+
+class TestEventAdapter extends EventAdapter {
+
+  override def fromJournal(event: Any, manifest: String): EventSeq = {
+    event match {
+      case e: TestPersistenceActor.Event => EventSeq.single(e)
+    }
+  }
+
+  override def manifest(event: Any): String = ""
+
+  override def toJournal(event: Any): Any = {
+    event match {
+      case e: TestPersistenceActor.Event.Untagged => e
+      case e: TestPersistenceActor.Event.Tagged   => Tagged(e, Set(e.tag))
+    }
+  }
+
+}
